45f10abde8760e225509bb2db751bbea1eda26c6
Julien Debray <julien.debray.pro@gmail.com>
2014-11-11 16:51:13 +0100

    base seeded and controller initalized

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
+
+
+10.times do
+  title = Faker::Lorem.sentence
+  content =  Faker::Lorem.paragraph(20)
+  article = Article.new(title: title, content: content)
+  article.save
+end
+
